22a4b4ddb21d0136c770c97d97a33b6286c987c9
Marek <ma.pavlik@seznam.cz>
2018-11-23 10:40:56 +0100

    Pridani assetu, click handler

--- app/src/main/java/com/example/marek/minesweeper/Cell.java
+++ app/src/main/java/com/example/marek/minesweeper/Cell.java
@@ -4,14 +4,17 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
+import android.view.View;
 
-public class Cell extends BaseCell {
+public class Cell extends BaseCell implements View.OnClickListener{
 
     public Cell(Context context, int position){
 
         super(context);
 
         setPosition(position);
+
+        setOnClickListener(this);
     }
 
     @Override
@@ -24,11 +27,88 @@ public class Cell extends BaseCell {
         super.onDraw(canvas);
 
         drawButton(canvas);
+
+
+        if(isFlagged())
+        {
+            drawFlag(canvas);
+        }
+        else if (isRevealed() && isBomb() && !isClicked())
+        {
+            drawBomb(canvas);
+        }
+        else
+        {
+            if (isClicked()){
+                if (getValue()==-1)
+                {
+                    drawBomb(canvas);
+                }
+                else
+                {
+                    drawNumber(canvas);
+                }
+            }
+            else
+            {
+                drawButton(canvas);
+            }
+        }
     }
+
+    private void drawFlag(Canvas canvas){
+        Drawable drawable = ContextCompat.getDrawable(getContext(), R.drawable.flag);
+        drawable.setBounds(0,0,getWidth(),getHeight());
+        drawable.draw(canvas);
+    }
+
 
     private void drawButton(Canvas canvas){
         Drawable drawable = ContextCompat.getDrawable(getContext(), R.drawable.unclicked);
         drawable.setBounds(0,0,getWidth(),getHeight());
         drawable.draw(canvas);
     }
+
+    private void drawBomb(Canvas canvas){
+        Drawable drawable = ContextCompat.getDrawable(getContext(), R.drawable.bomb);
+        drawable.setBounds(0,0,getWidth(),getHeight());
+        drawable.draw(canvas);
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+        GameEngine.getInstance().click(getXPos(),getYPos());
+    }
+
+    private void drawNumber(Canvas canvas){
+        Drawable drawable = null;
+
+        switch (getValue()){
+            case 0:
+                drawable = ContextCompat.getDrawable(getContext(),R.drawable.empty);break;
+            case 1:
+                drawable = ContextCompat.getDrawable(getContext(),R.drawable.one);break;
+            case 2:
+                drawable = ContextCompat.getDrawable(getContext(),R.drawable.two);break;
+            case 3:
+                drawable = ContextCompat.getDrawable(getContext(),R.drawable.three);break;
+            case 4:
+                drawable = ContextCompat.getDrawable(getContext(),R.drawable.four);break;
+            case 5:
+                drawable = ContextCompat.getDrawable(getContext(),R.drawable.five);break;
+            case 6:
+                drawable = ContextCompat.getDrawable(getContext(),R.drawable.six);break;
+            case 7:
+                drawable = ContextCompat.getDrawable(getContext(),R.drawable.seven);break;
+            case 8:
+                drawable = ContextCompat.getDrawable(getContext(),R.drawable.eight);break;
+
+        }
+
+        drawable.setBounds(0,0,getWidth(),getHeight());
+        drawable.draw(canvas);
+    }
+
 }
